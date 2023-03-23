595df52c72243add21e4bebf6bd444a453a91dfa
Peter Bob <1934182494@qq.com>
2023-03-23 13:12:31 +0800

    Add Plugin.cs

---
+++ MagicWorld/Plugins.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using BepInEx;
+
+namespace MagicWorld
+{
+    [BepInPlugin("magicworld", "Magic World","1.0.0")]
+    public class Plugins : BaseUnityPlugin
+    {
+        public readonly static string id = "magicworld";
+
+        public static bool inited = false;
+
+        public void OnEnable()
+        {
+            On.RainWorld.OnModsInit += RainWorld_OnModsInit;
+        }
+
+        private void RainWorld_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+        {
+            orig.Invoke(self);
+            try
+            {
+                Init();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+            }
+        }
+
+        public void Init()
+        {
+
+            inited = true;
+        }
+    }
+}
